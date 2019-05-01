37565fea0412bb50dba0c9290cbb453258d74426
Mason Brott <47792629+MasonBrott@users.noreply.github.com>
2019-05-01 09:24:59 -0800

    Inventory
    This defines the items class that holds the contents of your inventory.

---
+++ battle/classes/inventory.py
@@ -0,0 +1,7 @@
+class items:
+    def __init__(self, name, type, description, prop, amount):
+        self.name = name
+        self.type = type
+        self.description = description
+        self.prop = prop
+        self.amount = amount
