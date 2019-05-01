62aca7afcd037df20f81520a1da81c45546b015e
Mason Brott <47792629+MasonBrott@users.noreply.github.com>
2019-05-01 09:20:30 -0800

    Inventory addition
    Added a functional inventory with hp and mana potions as well as a sword weapon. Changes also coming to game.py and another file will be added, inventory.py.

--- battle/main.py
+++ battle/main.py
@@ -1,5 +1,6 @@
-from classes.game import person, bcolors
+rom classes.game import person, bcolors
 from classes.magic import spell
+from classes.inventory import *
 
 # create black magic
 fire = spell('fire', 8, 100, 'black')
@@ -12,9 +13,19 @@ quake = spell('quake', 14, 142, 'black')
 cure = spell('cure', 12, 120, 'white')
 cura = spell('cura', 18, 200, 'white')
 
+# instantiate inventory
+potion = items('potion', 'potion', 'This potion heals for 50 HP', 50, 5)
+mega_potion = items('mega potion', 'potion', 'This potion heals for 250 HP', 250, 2)
+mana_regen = items('mp regen', 'mana potion', 'This potion restores 20 MP', 20, 7)
+large_regen = items('large regen', 'mana potion', 'This potion restores 50 MP', 50, 5)
+
+claymore = items('claymore', 'weapon', 'This weapon hits for 100 HP', 100, 5)
+
 # instantiate people
-player = person(460, 65, 60, 34, [fire, thunder, blizzard, meteor, quake, cure, cura])
-enemy = person(1200, 65, 45, 25, [])
+player = person(460, 65, 60, 34,
+                [fire, thunder, blizzard, meteor, quake, cure, cura],
+                [potion, mega_potion, mana_regen, large_regen, claymore])
+enemy = person(1500, 65, 45, 25, [], [])
 
 running = True
 i = 0
@@ -27,10 +38,12 @@ while running:
     choice = input('Choose an action: ')
     index = int(choice) - 1
 
+    # defines the player attack
     if index == 0:
         dmg = player.generate_damage()
         enemy.take_damage(dmg)
         print('You attacked for', dmg)
+    # defines player magic attack or heal
     elif index == 1:
         player.choose_magic()
         magic_choice = int(input('Choose magic: ')) - 1
@@ -53,7 +66,42 @@ while running:
             enemy.take_damage(magic_damage)
             print(bcolors.OKBLUE + '\n' + spell.name + ' deals', str(magic_damage), 'points of damage' + bcolors.ENDC)
             print()
+    # defines item picking / damage or heals
+    elif index == 2:
+        player.choose_item()
+        item_choice = int(input('Choose Item: ')) - 1
 
+        item = player.items[item_choice]
+        if item.type == 'potion':
+            if item.amount == 0:
+                print('You are all out of ' + item.name + 's')
+                continue
+            player.hp += item.prop
+            if player.hp >= player.maxhp:
+                player.hp = player.maxhp
+            item.amount -= 1
+            print(bcolors.OKBLUE + '\n' + item.name + " heals for", str(item.prop), "HP." + bcolors.ENDC)
+            print(str(item.amount) + " " + item.name + 's remaining.')
+        elif item.type == 'mana potion':
+            if item.amount == 0:
+                print('You are all out of ' + item.name + 's')
+                continue
+            player.mp += item.prop
+            if player.mp >= player.maxmp:
+                player.mp = player.maxmp
+            item.amount -= 1
+            print(bcolors.OKBLUE + '\n' + item.name + " regenerates", str(item.prop), "MP." + bcolors.ENDC)
+            print(str(item.amount) + " " + item.name + 's remaining.')
+        elif item.type == 'weapon':
+            if item.amount == 0:
+                print('You are all out of ' + item.name + 's')
+                continue
+            enemy.hp -= item.prop
+            item.amount -= 1
+            print(bcolors.OKBLUE + '\n' + item.name + " deals", str(item.prop), "points of damage." + bcolors.ENDC)
+            print(str(item.amount) + " " + item.name + 's remaining.')
+
+    # defines enemy attack
     enemy_choice = 1
 
     enemy_damage = enemy.generate_damage()
@@ -66,6 +114,7 @@ while running:
     print('Your HP: ', bcolors.OKGREEN + str(player.get_hp()) + '/' + str(player.get_max_hp()) + bcolors.ENDC)
     print('Your MP: ', bcolors.OKBLUE + str(player.get_mp()) + '/' + str(player.get_max_mp()) + bcolors.ENDC + '\n')
 
+    # defines endgame requirements
     if enemy.get_hp() == 0:
         print(bcolors.OKGREEN + 'You Win!' + bcolors.ENDC)
         running = False
